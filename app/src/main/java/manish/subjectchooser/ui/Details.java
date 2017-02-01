d06ef857b763e1c1ee83e02d407552adb8fdec15
Manish Kumar <search.manish16@gmail.com>
2017-02-01 14:53:29 +0530

    Added new Activity detail to display items

---
+++ app/src/main/java/manish/subjectchooser/ui/Details.java
@@ -0,0 +1,15 @@
+package manish.subjectchooser.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import manish.subjectchooser.R;
+
+public class Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
